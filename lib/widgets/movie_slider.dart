af6804b7eff7ea25a6cf7add67e3f0734591b389
Ivan Gonzalez <ivan@iconos.mx>
2023-03-13 20:52:51 -0700

    creando movie slider

---
+++ lib/widgets/movie_slider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MovieSlider extends StatelessWidget {
+  const MovieSlider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 250,
+      color: Colors.red,
+    );
+  }
+}
