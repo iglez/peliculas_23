7f4b22c547395bd548ce275d06ee04736be25a73
Ivan Gonzalez <ivan@iconos.mx>
2023-03-22 17:18:59 -0700

    Cast Card y listandolos de manera horizontal

--- lib/widgets/casting_cards.dart
+++ lib/widgets/casting_cards.dart
@@ -10,6 +10,25 @@ class CastingCards extends StatelessWidget {
       width: double.infinity,
       height: 180,
       color: Colors.red,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (_, index) => _CastCard()
+      ),
+    );
+  }
+}
+
+class _CastCard extends StatelessWidget {
+  const _CastCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      width: 110,
+      height: 100,
+      color: Colors.green,
     );
   }
 }
