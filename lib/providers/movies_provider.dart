39dd577dd32977c2eff0c6e4350815af4c8bb88b
Ivan Gonzalez <ivan@iconos.mx>
2023-03-23 16:22:40 -0700

    Creando y cargando en una lista las "nowplaying"

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -2,12 +2,15 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:peliculas_23/models/movie.dart';
 import 'package:peliculas_23/models/now_playing_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   String _apiKey = '99568c3e97fec3b5e291a3ab8ab92f64';
   String _baseUrl = 'api.themoviedb.org';
   String _language = 'es-ES';
+
+  List<Movie> onDisplayMovies = [];
 
   MoviesProvider() {
     print('movie provider inicializado');
@@ -26,18 +29,9 @@ class MoviesProvider extends ChangeNotifier {
     // Await the http get response, then decode the json-formatted response.
     final response = await http.get(url);
 
-    print(response.statusCode);
-    // print(response.body);
-
-    // final Map<String, dynamic> decodedData = json.decode(response.body);
-    // print(decodedData);
-    // print(decodedData['dates']);
-    // print(decodedData['results'][0]);
+    // print(response.statusCode);
 
     final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
-
-    print( nowPlayingResponse.totalPages );
-    print( nowPlayingResponse.results[0].title );
-    print( nowPlayingResponse.results[0].overview );
+    onDisplayMovies = nowPlayingResponse.results;
   }
 }
