0771e666cc6b5ff979f3913a9862ac26a07c79e3
Ivan Gonzalez <ivan@iconos.mx>
2023-03-23 16:40:26 -0700

    desplegando la imagen

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -16,19 +16,23 @@ class CardSwiper extends StatelessWidget {
       height: size.height * 0.5,
       // color: Colors.red,
       child: Swiper(
-        itemCount: 10,
+        itemCount: movies.length,
         layout: SwiperLayout.STACK,
         itemWidth: size.width * 0.6,
         itemHeight: size.height * 0.4,
         itemBuilder: (context, index) {
+          final movie = movies[index];
+          // print(movie.posterPath);
+          // print(movie.fullPosterImg);
+
           return GestureDetector(
             onTap: () => Navigator.pushNamed(context, 'details',
                 arguments: 'movie-instance'),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
-              child: const FadeInImage(
-                placeholder: AssetImage('assets/no-image.jpg'),
-                image: NetworkImage('https://via.placeholder.com/300x400'),
+              child: FadeInImage(
+                placeholder: const AssetImage('assets/no-image.jpg'),
+                image: NetworkImage(movie.fullPosterImg),
                 fit: BoxFit.cover,
               ),
             ),
