627100b84838daaac8f659d92e525e8d9a5b07fa
Ivan Gonzalez <ivan@iconos.mx>
2023-03-22 20:09:00 -0700

    mandando llamar nuestro playing response

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:peliculas_23/models/now_playing_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   String _apiKey = '99568c3e97fec3b5e291a3ab8ab92f64';
@@ -28,9 +29,15 @@ class MoviesProvider extends ChangeNotifier {
     print(response.statusCode);
     // print(response.body);
 
-    final Map<String, dynamic> decodedData = json.decode(response.body);
-    print(decodedData);
-    print(decodedData['dates']);
-    print(decodedData['results'][0]);
+    // final Map<String, dynamic> decodedData = json.decode(response.body);
+    // print(decodedData);
+    // print(decodedData['dates']);
+    // print(decodedData['results'][0]);
+
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+
+    print( nowPlayingResponse.totalPages );
+    print( nowPlayingResponse.results[0].title );
+    print( nowPlayingResponse.results[0].overview );
   }
 }
