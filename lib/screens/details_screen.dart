ebc5b6a8f58d924d0bdb6ab2196d9c8cd9127cad
Ivan Gonzalez <ivan@iconos.mx>
2023-03-22 17:06:51 -0700

    Creando el overview

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -13,7 +13,8 @@ class DetailsScreen extends StatelessWidget {
           _CustomAppBar(),
           SliverList(
             delegate: SliverChildListDelegate([
-              _PosterAndTitle()
+              _PosterAndTitle(),
+              _Overview(),
             ])
           )
         ],
@@ -102,5 +103,14 @@ class _CustomAppBar extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  const _Overview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
   }
 }
