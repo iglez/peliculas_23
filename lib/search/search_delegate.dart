5287bf0ea667dac0fa8fc133fcb2afbf314204f6
Ivan Gonzalez <ivan@iconos.mx>
2023-04-10 18:21:33 -0700

    cargando el cascaron (fines didacticos)

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,29 +1,27 @@
-
 import 'package:flutter/material.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
   @override
   List<Widget>? buildActions(BuildContext context) {
     // TODO: implement buildActions
-    throw UnimplementedError();
+    return [Text('buildActions')];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
     // TODO: implement buildLeading
-    throw UnimplementedError();
+    return Text('buildLeading');
   }
 
   @override
   Widget buildResults(BuildContext context) {
     // TODO: implement buildResults
-    throw UnimplementedError();
+    return Text('buildResults');
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // TODO: implement buildSuggestions
-    throw UnimplementedError();
+    return Text('buildSuggestions');
   }
-
 }
